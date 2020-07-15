569854cfa0c6b1904eed77e2d6ed439d45f65502
Akshay Bahadur <akshaybahadur21@gmail.com>
2020-07-16 01:54:49 +0530

    adding feature selection, model fitting parameters

--- Basics/src/main/java/SparkML/_04_Feature_Selection.java
+++ Basics/src/main/java/SparkML/_04_Feature_Selection.java
@@ -13,18 +13,18 @@ import org.apache.spark.ml.tuning.TrainValidationSplitModel;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
 
 import java.util.Scanner;
 
-import static org.apache.spark.sql.functions.col;
-
-public class _03_Model_Fitting_Parameters {
+public class _04_Feature_Selection {
     /**
      *
-     * Model Fitting Parameters : This is in continuation to to our last module on House Sale Prediction
-     * We saw a WARN : WeightedLeastSquares
-     * Till now the LinearRegression class is a black box for us.
-     * We can provide these optional parameters and see which one works the best for us
+     * Feature Selection : This requires in-depth knowledge. We shall only look at the basics
+     *      - Eliminate Dependent variable
+     *      - Range of values of each variable (can use spark describe() method)
+     *      - Features which had good potential as predictors (High Correlation)
+     *      - removing duplicate features or highly correlated features
      *
      * */
     @SuppressWarnings("resource")
@@ -36,9 +36,26 @@ public class _03_Model_Fitting_Parameters {
                                         .getOrCreate();
         Dataset<Row> dataset = spark.read().option("header", true).option("inferSchema",true).csv("src\\main\\resources\\kc_house_data.csv");
 
+        dataset.describe().show();
+
+        /**
+         * Spark can let us know correlation between features
+         *      -1 and 1 mean high correlation
+         *      0 has no correlation
+         * Use stat() function
+         * */
+
+        dataset = dataset.drop("id", "date", "waterfront", "view", "condition", "grade", "yr_renovated", "zipcode", "lat", "long");
+
+        for(String col : dataset.columns())
+            System.out.println("Correlation b/w price and "+ col + " : " +  dataset.stat().corr("price", col));
+        dataset = dataset.drop("sqft_lot", "yr_built", "sqft_lot15, sqft_living15"); // removing these after getting low correlation
+
+        dataset = dataset.withColumn("sqft_above_precentage", col("sqft_above").divide("sqft_living"));
         VectorAssembler vectorAssembler = new VectorAssembler()
-                        .setInputCols(new String[]{"bedrooms", "bathrooms", "sqft_living", "sqft_lot", "floors", "grade"})
-                        .setOutputCol("features");
+                .setHandleInvalid("skip")
+                .setInputCols(new String[]{"bedrooms", "bathrooms", "sqft_living", "sqft_above_precentage", "floors"})
+                .setOutputCol("features");
         dataset = vectorAssembler.transform(dataset);
 
         Dataset<Row> inputDataset = dataset.select(col("features"), col("price")).withColumnRenamed("price", "label");
@@ -47,32 +64,17 @@ public class _03_Model_Fitting_Parameters {
         Dataset<Row> trainData = trainTestData[0];
         Dataset<Row> testData = trainTestData[1];
 
-        /**
-         * We can specify a grid of values and spark will the best one out of them using ParamGridBuilder
-         * We also need to split the entire dataset to train : test : validate.
-         * Spark will do that for use. We need to create a TrainValidationSplit Object
-         *      - estimator : Model which we are using.
-         *      - evaluator : how to determine the best model
-         *      - param Map : the map for all the possible values for param
-         *      - train ratio : what proportion to split into training and test.
-         * */
-
-//        LinearRegression linearRegression = new LinearRegression()
-//                                                .setMaxIter(10)
-//                                                .setRegParam(0.3)
-//                                                .setElasticNetParam(0.8);
-
         LinearRegression linearRegression = new LinearRegression();
         ParamGridBuilder paramGridBuilder = new ParamGridBuilder();
         ParamMap[] paramMaps = paramGridBuilder.addGrid(linearRegression.regParam(), new double[]{0.01, 0.1, 0.5})
-                                                .addGrid(linearRegression.elasticNetParam(), new double[]{0.0, 0.5, 1.0})
-                                                .build();
+                .addGrid(linearRegression.elasticNetParam(), new double[]{0.0, 0.5, 1.0})
+                .build();
 
         TrainValidationSplit trainValidationSplit = new TrainValidationSplit()
-                                                        .setEstimator(linearRegression)
-                                                        .setEvaluator(new RegressionEvaluator().setMetricName("r2"))
-                                                        .setEstimatorParamMaps(paramMaps)
-                                                        .setTrainRatio(0.8);
+                .setEstimator(linearRegression)
+                .setEvaluator(new RegressionEvaluator().setMetricName("r2"))
+                .setEstimatorParamMaps(paramMaps)
+                .setTrainRatio(0.8);
 
         TrainValidationSplitModel splitModel = trainValidationSplit.fit(trainData);
         LinearRegressionModel model = (LinearRegressionModel) splitModel.bestModel();
@@ -83,7 +85,6 @@ public class _03_Model_Fitting_Parameters {
 
         System.out.println("R2 value for test data : " + model.evaluate(testData).r2());
         System.out.println("RMSE value for test data : " + model.evaluate(testData).rootMeanSquaredError());
-
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
         spark.close();
